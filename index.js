b5b3b7962c1d5d943ec83a3588e691a3a5c02a1d
Abhishek kumar <115467885+dev-abhishek-k@users.noreply.github.com>
2026-08-11 10:47:22 +0530

    Fix CORS origin URL by adding trailing slash

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT=process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());    
 app.use(cors({
-  origin: "https://todo-frontend-blush-psi.vercel.app",
+  origin: "https://todo-frontend-blush-psi.vercel.app/",
   // origin: "http://localhost:5173",
   credentials: true,
 }));
